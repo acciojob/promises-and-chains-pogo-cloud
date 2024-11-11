9b02bbf11adbf64ade94ad9b4af713d57a15d042
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-11-11 17:40:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-//your JS code here. If required.
-  let form=document.querySelector("#form-2");
-      form.addEventListener("submit",(event)=>{
+let formButton=document.querySelector("#btn");
+      formButton.addEventListener("click",(event)=>{
         event.preventDefault();
           let age=document.getElementById("age").value;
           let name=document.getElementById("name").value;
           if(!age||!name){
             
-            alert("Please enter valid details);
+            alert("Please enter valid details");
           }else{
-            //console.log("form is submitted");
+            console.log("form is submitted");
             const p=new Promise((resolve,reject)=>{
                  setTimeout(()=>{
                      if(age>18){
                        resolve(`Welcome, ${name}. You can vote.`)
-                     }else if(age<=18){
+                     }else{
                         reject(`Oh sorry ${name}. You aren't old enough`)
                      }
                  },4000)

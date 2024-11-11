ac033950a1f9ac6c3f6496766835d91755ae12df
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-11-11 16:32:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+  let form=document.querySelector("#form-2");
+      form.addEventListener("submit",(event)=>{
+        event.preventDefault();
+          let age=document.getElementById("age").value;
+          let name=document.getElementById("name").value;
+          if(!age||!name){
+            
+            alert("Please fill the information");
+          }else{
+            console.log("form is submitted");
+            const p=new Promise((resolve,reject)=>{
+                 setTimeout(()=>{
+                     if(age>18){
+                       resolve(`Welcome, ${name}. You can vote.`)
+                     }else if(age<=18){
+                        reject(`Oh sorry ${name}. You aren't old enough`)
+                     }
+                 },4000)
+            })
+            p.then((post)=>{
+            alert(post);
+          }).catch((err)=>{
+             alert(err);
+          })
+          }
+         
+          
+      })
